4d0da911496d3d5fc938dad7de43dd906ef331eb
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-09-10 12:32:36 +0900

    lambda : 함수 호출을 함수 인자로 바꿔줌
    onkey가 move(n)을 바로 실행시키는 문제를 lambda를 통해 방지하였다

---
+++ DRILL_01.py
@@ -0,0 +1,24 @@
+import turtle
+
+#90 -> 0:오른쪽 1:윗쪽, 2:왼쪽, 3:아래쪽
+def move(n):
+    turtle.stamp()
+    turtle.setheading(90 * n)
+    turtle.forward(50)
+    
+    
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+#( 90 * n )-> 0:오른쪽 1:윗쪽, 2:왼쪽, 3:아래쪽
+#lambda : 함수를 인자로 전달해야 하는 경우 사용
+turtle.onkey(lambda: move(1), 'w')
+turtle.onkey(lambda: move(2), 'a')
+turtle.onkey(lambda: move(3), 's')
+turtle.onkey(lambda: move(0), 'd')
+turtle.onkey(restart, 'Escape')
+
+turtle.listen()
+turtle.exitonclick()
